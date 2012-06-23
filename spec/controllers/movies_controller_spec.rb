65cffa89a13bc280b566293ea996f1827aa3b70d
Geoff Longman <glongman@gmail.com>
2012-06-23 11:31:15 -0400

    movies controller spec - 1st cut

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
-describe "MoviesController" do
+describe MoviesController do
   describe "add director to existing movie" do
     before :each do
       @movie = FactoryGirl.create :movie, :director => nil
     end
-    it 'should render the Movie edit page' do
-      get :edit, @movie
-      response.should render_template 'edit'
+    describe "from the edit page" do
+      it 'should render the Movie edit page' do
+        get :edit, id: @movie.id
+        response.should render_template 'edit'
+      end
+    end
+    describe "upon post with director non nil" do
+      it 'should set the director' do
+        post :update, id: @movie.id, movie: {'director' => 'George Lucas'} 
+        assigns[:movie].director.should == 'George Lucas'
+      end
     end
   end
   describe "find movie with same director" do
+
   end
   describe "can't find similar movies if we don't know director" do
   end
